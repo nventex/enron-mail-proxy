7cf870559a329f5a7e31b838efc5eb540fa805cf
Nventex <nventex@gmail.com>
2017-02-26 20:40:50 -0600

    add support for get email

--- app.js
+++ app.js
@@ -18,16 +18,34 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.post("/api/search", (req, resp) => {
-    sendToElasticSearch(req.body, resp);
+    searchByTemplate(req.body, resp);
 });
 
-function sendToElasticSearch(searchData, resp) {
+app.get("/api/email/:id", (req, resp) => {
+    getEmailById(req.params.id, resp);
+});
+
+function getEmailById(id, resp) {
+    var options = {
+        "hostname": process.env.hostName,
+        "port": "9200",
+        "path": `/enron_emails/email/${id}`,
+        "method": "GET",
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    }    
+
+    sendRequest(options, resp, null);
+}
+
+function searchByTemplate(searchData, resp) {
     var postData = JSON.stringify(searchData);
     
     var options = {
         "hostname": process.env.hostName,
         "port": "9200",
-        "path": "/enron_emails/_search",
+        "path": "/enron_emails/_search/template",
         "method": "POST",
         "headers": {
             "Content-Type": "application/json",
@@ -35,6 +53,10 @@ function sendToElasticSearch(searchData, resp) {
         }
     }
 
+    sendRequest(options, resp, postData);
+}
+
+function sendRequest(options, resp, postData) {
     var req = http.request(options, res => {
         res.setEncoding("utf8");
 
@@ -53,8 +75,11 @@ function sendToElasticSearch(searchData, resp) {
         });
     });
 
-    req.write(postData);
-    req.end();
+    if (postData) {
+        req.write(postData);
+    }
+    
+    req.end();    
 }
 
 app.listen(port);
